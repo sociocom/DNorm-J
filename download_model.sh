bf21afd790eb572dfaa43eb8d8facb09bd17bc07
Shogo Ujiie <suzzz428@gmail.com>
2020-05-13 16:36:18 +0900

    cleanup

--- download_model.sh
+++
@@ -1,7 +0,0 @@
-if [ -d models ]; then
-    echo "path ./models is already exists"
-else
-    mkdir models
-    wget http://aoi.naist.jp/DNorm/W.npz -O models/W.npz
-    wget http://aoi.naist.jp/DNorm/DATA_IM_tfidf.pkl -O models/DATA_IM_tfidf.pkl
-fi
