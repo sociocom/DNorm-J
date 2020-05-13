87f1c86a11ef1722ef83ef66192faf8457293b88
Shogo Ujiie <suzzz428@gmail.com>
2020-05-14 00:49:20 +0900

    add shell script that predict normal disease name using pretrained model

--- download_model.sh
+++ download_model.sh
@@ -2,6 +2,6 @@ if [ -d models ]; then
     echo "path ./models is already exists"
 else
     mkdir models
-    wget http://aoi.naist.jp/DNorm/W.npz -O models/W.npz
+    wget http://aoi.naist.jp/DNorm/W_EHR_all.npz -O models/W_EHR_all.npz
     wget http://aoi.naist.jp/DNorm/EHR_idf.pkl -O models/EHR_idf.pkl
 fi
