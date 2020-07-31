f663b9eb47fc6ada69c9a700d1b69e280b80b14d
Shogo Ujiie <suzzz428@gmail.com>
2020-08-01 04:40:15 +0900

    add stdin stdout

--- dnorm_j/__main__.py
+++ dnorm_j/__main__.py
@@ -1,22 +1,27 @@
 import argparse
+import sys
 
 from .d_norm import DNorm
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input_file", help="train path")
+parser.add_argument("-i", '--input_file', nargs='?', type=argparse.FileType(),
+        default=sys.stdin, help="input file path")
+parser.add_argument("-o", '--output_file', nargs='?', type=argparse.FileType(),
+        default=sys.stdout, help="output file path")
 parser.add_argument("-n", "--normal", default="default", help="normal set path")
 parser.add_argument("-d", "--dict", default="default", help="normal set path")
-parser.add_argument("-o", "--output_file", help="output model path (pkl)")
 args = parser.parse_args()
 
-with open(args.input_file, 'r') as f:
+#with open(args.input_file, 'r') as f:
+with args.input_file as f:
     test_x = [line for line in f.read().split('\n') if line != '']
 
 model = DNorm.from_pretrained(args.normal, args.dict)
 res = [model.normalize(x) for x in test_x]
 
-with open(args.output_file, 'w') as f:
-    f.write('\n'.join(res))
+#with open(args.output_file, 'w') as f:
+with args.output_file as f:
+    f.write('\n'.join(res) + '\n')
 
 
